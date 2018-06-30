7e33894d7915acbc40b83fb3336960a214f00955
TomHome <39772727+TomParkwood@users.noreply.github.com>
2018-06-30 20:25:09 +0100

    First Push

---
+++ PrintingExcercise.py
@@ -0,0 +1,10 @@
+#personal message
+FirstName ="Teh"
+SecondName = "Fatpie"
+FullName = FirstName + " " + SecondName
+Text = " would you like to learn Python today?"
+Message = "Hello " + FullName + Text
+print(Message)
+
+#Name Cases
+#Store a name in a variable, and h
---
+++ Whitespace.py
@@ -0,0 +1,10 @@
+#Whitespace
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJava")
+print("Languages:\n\tPython\n\tC\n\tJava")
+Language = " Python "
+print(Language)
+print(Language.rstrip())
+print(Language.lstrip())
+print(Language.strip())
